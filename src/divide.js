74c50a99693bbfe0047950a437e00144722951a4
Eka Risyana <risyana@gmail.com>
2019-08-02 18:18:21 +0700

    feat(divide): add 'NumOfDigit' parameter

--- src/divide.js
+++ src/divide.js
@@ -2,12 +2,13 @@ export default divide
 
 /**
  * @module Divide
- * @Description This method will perform Divide operation.
+ * @Description This method will perform Divide operation, with certain number of digit after decimal
  *
  * @param {Number} div1 - the number that is divided, divident
  * @param {Number} div2 - the number to be divided with, divisor
+ * @param {Number} NumOfDigit  - the number of digit after decimal
  * @return {Number} - Result of Division
  */
-function divide(div1, div2) {
-  return div1 / div2
+function divide(div1, div2, NumOfDigit) {
+  return (div1 / div2).toFixed(NumOfDigit)
 }
--- test/divide-test.js
+++ test/divide-test.js
@@ -4,7 +4,8 @@ import {divide} from '../src'
 test('divide two numbers ', t => {
   const number1 = 7
   const number2 = 2
-  const expected = 7 / 2
-  const actual = divide(number1, number2)
+  const numOfDigit = 2
+  const expected = (7 / 2).toFixed(2)
+  const actual = divide(number1, number2, numOfDigit)
   t.deepEqual(actual, expected)
 })
